b3b587ca6bb4cb7e08bebcc3ef800ee738998ce8
Eric Ely <eric.j.ely@gmail.com>
2015-10-10 00:00:40 -0400

    change the bootloader to work as it did before we upgraded

--- bootloader/src/bluz/main.c
+++ bootloader/src/bluz/main.c
@@ -51,6 +51,8 @@
 #include "nrf_mbr.h"
 #include "sst25vf_spi.h"
 #include "hw_layout.h"
+#include "nrf_delay.h"
+#include "pstorage.h"
 
 #define IS_SRVC_CHANGED_CHARACT_PRESENT 1                                                       /**< Include the service_changed characteristic. For DFU this should normally be the case. */
 
@@ -87,8 +89,13 @@ void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
  */
 static void leds_init(void)
 {
-    nrf_gpio_cfg_output(UPDATE_IN_PROGRESS_LED);
-    nrf_gpio_pin_set(UPDATE_IN_PROGRESS_LED);
+    nrf_gpio_cfg_output(RGB_LED_PIN_RED);
+    nrf_gpio_cfg_output(RGB_LED_PIN_GREEN);
+    nrf_gpio_cfg_output(RGB_LED_PIN_BLUE);
+    
+    nrf_gpio_pin_set(RGB_LED_PIN_RED);
+    nrf_gpio_pin_set(RGB_LED_PIN_GREEN);
+    nrf_gpio_pin_set(RGB_LED_PIN_BLUE);
 }
 
 
@@ -122,6 +129,44 @@ static void buttons_init(void)
 static void sys_evt_dispatch(uint32_t event)
 {
     pstorage_sys_event_handler(event);
+}
+
+/**@brief Function for handling callbacks from pstorage module.
+ *
+ * @details Handles pstorage results for clear and storage operation. For detailed description of
+ *          the parameters provided with the callback, please refer to \ref pstorage_ntf_cb_t.
+ */
+bool stillWorking = false;
+static void pstorage_callback_handler(pstorage_handle_t * p_handle,
+                                      uint8_t             op_code,
+                                      uint32_t            result,
+                                      uint8_t           * p_data,
+                                      uint32_t            data_len)
+{
+    switch (op_code)
+    {
+        case PSTORAGE_STORE_OP_CODE:
+            
+            break;
+            
+        case PSTORAGE_CLEAR_OP_CODE:
+            
+            if (result == NRF_SUCCESS)
+            {
+                stillWorking = false;
+                return;
+            }
+            else
+            {
+                stillWorking = false;
+                return;
+            }
+            break;
+            
+        default:
+            break;
+    }
+    APP_ERROR_CHECK(result);
 }
 
 
@@ -173,13 +218,24 @@ static void scheduler_init(void)
     APP_SCHED_INIT(SCHED_MAX_EVENT_DATA_SIZE, SCHED_QUEUE_SIZE);
 }
 
+void blink(int times)
+{
+    for (int i = 0; i < times; i++) {
+        nrf_gpio_pin_clear(RGB_LED_PIN_BLUE);
+        nrf_delay_ms(100);
+        nrf_gpio_pin_set(RGB_LED_PIN_BLUE);
+        nrf_delay_ms(100);
+    }
+    nrf_delay_ms(450);
+}
+
 
 /**@brief Function for bootloader main entry.
  */
 int main(void)
 {
     uint32_t err_code;
-    bool     dfu_start = false;
+//    bool     dfu_start = false;
     bool     app_reset = (NRF_POWER->GPREGRET == BOOTLOADER_DFU_START);
 
     if (app_reset)
@@ -197,44 +253,112 @@ int main(void)
     // Initialize.
     timers_init();
     buttons_init();
-
-    (void)bootloader_init();
-
-    if (bootloader_dfu_sd_in_progress())
-    {
-        nrf_gpio_pin_clear(UPDATE_IN_PROGRESS_LED);
-
-        err_code = bootloader_dfu_sd_update_continue();
-        APP_ERROR_CHECK(err_code);
-
-        ble_stack_init(!app_reset);
-        scheduler_init();
-
-        err_code = bootloader_dfu_sd_update_finalize();
-        APP_ERROR_CHECK(err_code);
-
-        nrf_gpio_pin_set(UPDATE_IN_PROGRESS_LED);
-    }
-    else
-    {
-        // If stack is present then continue initialization of bootloader.
-        ble_stack_init(!app_reset);
-        scheduler_init();
-    }
-
-    dfu_start  = app_reset;
-    dfu_start |= ((nrf_gpio_pin_read(BOOTLOADER_BUTTON) == 0) ? true: false);
     
-    if (dfu_start || (!bootloader_app_is_valid(DFU_BANK_0_REGION_START)))
-    {
-        nrf_gpio_pin_clear(UPDATE_IN_PROGRESS_LED);
-
-        // Initiate an update of the firmware.
-        err_code = bootloader_dfu_start();
+    //For now, until you implement the full bootloader
+    ble_stack_init(true);
+    
+    //init external flash then check if update is ready
+    sFLASH_Init();
+    uint8_t byte0 = sFLASH_ReadSingleByte(FLASH_FW_STATUS);
+    if (byte0 == 0x01) {
+        //we have an app waiting for us. let's first find out the length
+        uint32_t fw_len = 0;
+        uint8_t byte1 = sFLASH_ReadSingleByte(FLASH_FW_LENGTH1);
+        uint8_t byte2 = sFLASH_ReadSingleByte(FLASH_FW_LENGTH2);
+        uint8_t byte3 = sFLASH_ReadSingleByte(FLASH_FW_LENGTH3);
+        fw_len = (byte1 << 16) | (byte2 << 8)  |  byte3;
+        
+        //let's init the pstorage
+        pstorage_handle_t m_storage_handle_app;
+        pstorage_module_param_t storage_module_param = {.cb = pstorage_callback_handler};
+        
+        storage_module_param.block_size = 0x100;
+        storage_module_param.block_count = fw_len / 256;
+        
+        pstorage_init();
+        err_code = pstorage_raw_register(&storage_module_param, &m_storage_handle_app);
         APP_ERROR_CHECK(err_code);
-
-        nrf_gpio_pin_set(UPDATE_IN_PROGRESS_LED);
+        
+        m_storage_handle_app.block_id  = DFU_BANK_0_REGION_START;
+        
+        uint32_t    ops_count = 7;
+        //now clear the nrf51 flash
+        stillWorking = true;
+        err_code = pstorage_raw_clear(&m_storage_handle_app, fw_len);
+        do {
+            app_sched_execute();
+            pstorage_access_status_get(&ops_count);
+        }
+        while(ops_count != 0);
+        
+        app_sched_execute();
+        
+        APP_ERROR_CHECK(err_code);
+        
+        //now read from SPI Flash one page at a time and copy over to internal flash
+        uint8_t buf[PSTORAGE_FLASH_PAGE_SIZE];
+        uint32_t addr = FLASH_FW_ADDRESS;
+        for (int i = 0; i < fw_len; i+=PSTORAGE_FLASH_PAGE_SIZE) {
+            sFLASH_ReadBuffer(buf, addr, PSTORAGE_FLASH_PAGE_SIZE);
+            err_code = pstorage_raw_store(&m_storage_handle_app,
+                                          (uint8_t *)buf,
+                                          PSTORAGE_FLASH_PAGE_SIZE,
+                                          i);
+            APP_ERROR_CHECK(err_code);
+            
+            do {
+                app_sched_execute();
+                pstorage_access_status_get(&ops_count);
+            }
+            while(ops_count != 0);
+            
+            
+            addr+=PSTORAGE_FLASH_PAGE_SIZE;
+        }
+        sFLASH_EraseSector(FLASH_FW_STATUS);
+        sFLASH_WriteSingleByte(FLASH_FW_STATUS, 0x00);
     }
+    //TO DO: Temporary for now, just boot directly into the app.
+    //Really, we should go on and see if they want to enter boot mode, then take FW and keys through DFU
+    bootloader_app_start(DFU_BANK_0_REGION_START);
+
+//    (void)bootloader_init();
+//
+//    if (bootloader_dfu_sd_in_progress())
+//    {
+//        nrf_gpio_pin_clear(UPDATE_IN_PROGRESS_LED);
+//
+//        err_code = bootloader_dfu_sd_update_continue();
+//        APP_ERROR_CHECK(err_code);
+//
+//        ble_stack_init(!app_reset);
+//        scheduler_init();
+//
+//        err_code = bootloader_dfu_sd_update_finalize();
+//        APP_ERROR_CHECK(err_code);
+//
+//        nrf_gpio_pin_set(UPDATE_IN_PROGRESS_LED);
+//    }
+//    else
+//    {
+//        // If stack is present then continue initialization of bootloader.
+//        ble_stack_init(!app_reset);
+//        scheduler_init();
+//    }
+//
+//    dfu_start  = app_reset;
+//    dfu_start |= ((nrf_gpio_pin_read(BOOTLOADER_BUTTON) == 0) ? true: false);
+//    
+//    if (dfu_start || (!bootloader_app_is_valid(DFU_BANK_0_REGION_START)))
+//    {
+//        nrf_gpio_pin_clear(UPDATE_IN_PROGRESS_LED);
+//
+//        // Initiate an update of the firmware.
+//        err_code = bootloader_dfu_start();
+//        APP_ERROR_CHECK(err_code);
+//
+//        nrf_gpio_pin_set(UPDATE_IN_PROGRESS_LED);
+//    }
 
     if (bootloader_app_is_valid(DFU_BANK_0_REGION_START) && !bootloader_dfu_sd_in_progress())
     {
