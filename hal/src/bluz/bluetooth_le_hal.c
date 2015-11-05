b4f0663f598de83aa1926b9830e49ee12b84c6d0
Eric Ely <eric.j.ely@gmail.com>
2015-11-05 13:58:14 -0500

    put our own copyright on all the files we implemented

--- hal/src/bluz/bluetooth_le_hal.c
+++ hal/src/bluz/bluetooth_le_hal.c
@@ -1,26 +1,18 @@
 /**
- ******************************************************************************
- * @file    bluetooth_ls_hal.c
- * @author  Eric Ely
- * @version V1.0.0
- * @date    1-October-2015
- * @brief
- ******************************************************************************
-  Copyright (c) 2013-2015 Easier to Use, LLC  All rights reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation, either
-  version 3 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
+ Copyright (c) 2015 MidAir Technology, LLC.  All rights reserved.
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, either
+ version 3 of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /* Includes ------------------------------------------------------------------*/
--- platform/MCU/NRF51/SPARK_Firmware_Driver/inc/hw_config.h
+++ platform/MCU/NRF51/SPARK_Firmware_Driver/inc/hw_config.h
@@ -1,26 +1,18 @@
 /**
- ******************************************************************************
- * @file    nrf_hw_config.h
- * @author  Eric Ely, Satish Nair, Zachary Crockett and Mohit Bhoite
- * @version V1.0.0
- * @date    8-October-2014
- * @brief   Hardware Configuration & Setup. Modified from Spark FW
- ******************************************************************************
-  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation, either
-  version 3 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
-  ******************************************************************************
+ Copyright (c) 2015 MidAir Technology, LLC.  All rights reserved.
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, either
+ version 3 of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
