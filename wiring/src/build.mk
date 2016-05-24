ab62e4114110d3074dd67bbf4d382e0a4994522b
Bryan J. Rentoul (aka Gruvin) <gruvin@gmail.com>
2016-05-25 11:30:17 +1200

    Proposed makefile to selectively choose wiring bluz_*.cpp over spark_*.cpp
    $eric — This is the most eloquent method I could come up with. What do
    you think?

--- wiring/src/build.mk
+++ wiring/src/build.mk
@@ -9,8 +9,25 @@ TARGET_SRC_PATH = src
 # C source files included in this build.
 CSRC +=
 
-# enumerate target cpp files
-CPPSRC += $(call target_files,src/,*.cpp)
+# enumerate target cpp files into temporary variable, skipping potential bluz_*.cpp files
+CPPSRC_T = $(call target_files,src/,spark_*.cpp) # NOTE: spark_*.cpp (was *.cpp)
+
+# If we building the Bluz platform ...
+# replace specific spark_wiring sources with bluz_wiring sources.
+# We do this to avoid lot of #ifdefs over large code block in the individual files
+ifeq ("PLATFORM_ID","103")
+else ("PLATFORM_ID","269") # this is how we OR in gnu make
+  PLATFORM_BLUZ = true
+endif
+ifeq ($(PLATFORM_BLUZ),true)
+  # change out each file as required
+  CPPSRC += $(CPPSRC_T:src/spark_wiring_timer.cpp=src/bluz_wiring_timer.cpp) 
+# maybe later...
+# CPPSRC += $(CPPSRC_T:src/spark_wiring_tcpclient.cpp=src/bluz_wiring_tcpclient.cpp) 
+else
+  # not building Bluz, so run with the original CPPSRC list
+  CPPSRC += $(CPPSRC_T)
+endif
 
 # ASM source files included in this build.
 ASRC +=
