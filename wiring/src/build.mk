bd6d0e7785271a8fcbba5515df5efd15680666b3
Bryan J. Rentoul (aka Gruvin) <gruvin@gmail.com>
2016-05-25 13:26:28 +1200

    This is much better ...
    … this time any bluz_wiring* files will replace spark_wiring* versions
    if they exist. Meanwhile, the bluz_* version is added regardless.
    
    So t his one works with the way things are now, without having to
    change Particle’s code by splitting spark_wiring_timer.h out into their
    .cpp/.h pair.

--- wiring/src/build.mk
+++ wiring/src/build.mk
@@ -9,25 +9,15 @@ TARGET_SRC_PATH = src
 # C source files included in this build.
 CSRC +=
 
-# enumerate target cpp files into temporary variable, skipping potential bluz_*.cpp files
-CPPSRC_T = $(call target_files,src/,spark_*.cpp) # NOTE: spark_*.cpp (was *.cpp)
-
-# If we building the Bluz platform ...
-# replace specific spark_wiring sources with bluz_wiring sources.
-# We do this to avoid lot of #ifdefs over large code block in the individual files
-ifeq ("PLATFORM_ID","103")
-else ("PLATFORM_ID","269") # this is how we OR in gnu make
-  PLATFORM_BLUZ = true
-endif
-ifeq ($(PLATFORM_BLUZ),true)
-  # change out each file as required
-  CPPSRC += $(CPPSRC_T:src/spark_wiring_timer.cpp=src/bluz_wiring_timer.cpp) 
-# maybe later...
-# CPPSRC += $(CPPSRC_T:src/spark_wiring_tcpclient.cpp=src/bluz_wiring_tcpclient.cpp) 
+ifeq (,$(filter "PLATFORM_ID","103" "269"))
+  SPARK_CPPSRC = $(call target_files,src/,spark_*.cpp)
+  BLUZ_CPPSRC = $(call target_files,src/,bluz_*.cpp)
+  CPPSRC += $(filter-out $(subst src/bluz_,src/spark_,$(BLUZ_CPPSRC)),$(SPARK_CPPSRC))
+  CPPSRC += $(BLUZ_CPPSRC)
 else
-  # not building Bluz, so run with the original CPPSRC list
-  CPPSRC += $(CPPSRC_T)
+  CPPSRC += $(SPARK_CPPSRC)
 endif
+$(warning VVVVVVVVVVVVVVVVVVVVVVVVVVVV CPPSRC=$(CPPSRC) )
 
 # ASM source files included in this build.
 ASRC +=
