47527a59124dbe9b1335a7c2ca888687349a226a
Bryan J. Rentoul (aka Gruvin) <gruvin@gmail.com>
2016-06-01 12:24:51 +1200

    Turned DEBUG on in tcpclient test app.

--- user/applications/tcpclient/tcpclient.cpp
+++ user/applications/tcpclient/tcpclient.cpp
@@ -1,5 +1,7 @@
 #include "application.h"
 
+Serial1DebugOutput debugOutput(38400); // default is 9600 and log everything
+
 TCPClient client;
 IPAddress ip = { 10, 0, 1, 112 };
     
@@ -10,10 +12,12 @@ void setup()
 void loop()
 {
   static bool oneshot = true;
+  static uint32_t t = millis(); 
   if (oneshot && Particle.connected())
   {
     Particle.publish("TCPClient Test", "ONLINE");
     oneshot=false;
+    t = millis();
     if (client.connect(ip, 22))
     {
       Particle.publish("TCP connect", "success!");
@@ -23,7 +27,20 @@ void loop()
       Particle.publish("TCP connect", "failed :-(");
     }
   }
-
+  if (!oneshot && millis() > (t + 5000)) { 
+    t = millis(); Particle.publish("PING"); 
+/*
+    String s = "";
+    int bytes;
+    while ( (bytes = client.available()) ) s += client.read();
+    if (s.length()) { 
+      char sbuf[1024]; 
+      s.toCharArray(sbuf, sizeof(sbuf));
+      DEBUG("TCP_DATA: %s", sbuf); 
+      s = "";
+    }
+*/
+  }
 }
 
 
