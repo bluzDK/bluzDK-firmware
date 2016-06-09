07cf325dcdf5877006abd25730041e0744ddb1a6
Bryan J. Rentoul (aka Gruvin) <gruvin@gmail.com>
2016-06-10 11:36:46 +1200

    Fix wiring/src/build.mk

--- wiring/src/build.mk
+++ wiring/src/build.mk
@@ -9,15 +9,17 @@ TARGET_SRC_PATH = src
 # C source files included in this build.
 CSRC +=
 
+SPARK_CPPSRC = $(call target_files,$(shell find src ! -name bluz_*.cpp -name *.cpp))
 ifeq ($(PLATFORM_ID),$(filter $(PLATFORM_ID),103 269))
-  SPARK_CPPSRC = $(call target_files,src/,[!bluz_]*.cpp)
+  # replace any spark_.cpp files with bluz_*.cpp versions, if present
   BLUZ_CPPSRC = $(call target_files,src/,bluz_*.cpp)
   CPPSRC += $(filter-out $(subst src/bluz_,src/spark_,$(BLUZ_CPPSRC)),$(SPARK_CPPSRC))
   CPPSRC += $(BLUZ_CPPSRC)
 else
   CPPSRC += $(SPARK_CPPSRC)
 endif
-#$(warning XXX_DEBUG_XXX CPPSRC=$(CPPSRC))
+#$(info AAA_DEBUG_AAA SPARK_CPPSRC=$(SPARK_CPPSRC))
+#$(info XXX_DEBUG_XXX CPPSRC=$(CPPSRC))
 
 # ASM source files included in this build.
 ASRC +=
