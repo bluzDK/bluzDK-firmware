040c6b77e958f2377a77c160418afbd90357f0be
Eric Ely <eric.j.ely@gmail.com>
2016-07-16 14:29:37 -0400

    allow connection intervals up to 300 mSec

--- platform/MCU/NRF51/SPARK_Firmware_Driver/inc/nrf51_config.h
+++ platform/MCU/NRF51/SPARK_Firmware_Driver/inc/nrf51_config.h
@@ -61,7 +61,7 @@
 
 #define DEVICE_NAME                     "Bluz DK"	                            	/**< Name of device. Will be included in the advertising data. */
 #define MIN_CONN_INTERVAL               MSEC_TO_UNITS(7.5, UNIT_1_25_MS)            /**< Minimum acceptable connection interval (0.5 seconds). */
-#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(40, UNIT_1_25_MS)           /**< Maximum acceptable connection interval (1 second). */
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(300, UNIT_1_25_MS)           /**< Maximum acceptable connection interval (1 second). */
 #define SLAVE_LATENCY                   0                                           /**< Slave latency. */
 #define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds). */
 
