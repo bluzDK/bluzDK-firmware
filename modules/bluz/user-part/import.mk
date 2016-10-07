d3ddf0e17f8c783db8db627f0529d459f5f64f38
Eric Ely <eric.j.ely@gmail.com>
2016-10-06 22:52:47 -0400

    bump module versions so new fixes get picked up

--- modules/bluz/user-part/import.mk
+++ modules/bluz/user-part/import.mk
@@ -1,4 +1,4 @@
-USER_PART_MODULE_VERSION ?= 6
+USER_PART_MODULE_VERSION ?= 7
 USER_PART_MODULE_PATH ?= $(PROJECT_ROOT)/modules/bluz/user-part
 include $(call rwildcard,$(USER_PART_MODULE_PATH)/,include.mk)
 
