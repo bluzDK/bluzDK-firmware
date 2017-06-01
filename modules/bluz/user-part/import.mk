856ee277a79d303215a32196fa0c5e4b096a2f3c
Eric Ely <eric.j.ely@gmail.com>
2017-06-01 09:00:00 -0400

    bump versions

--- modules/bluz/user-part/import.mk
+++ modules/bluz/user-part/import.mk
@@ -1,4 +1,4 @@
-USER_PART_MODULE_VERSION ?= 11
+USER_PART_MODULE_VERSION ?= 12
 USER_PART_MODULE_PATH ?= $(PROJECT_ROOT)/modules/bluz/user-part
 include $(call rwildcard,$(USER_PART_MODULE_PATH)/,include.mk)
 
